72df79cdd6a52490867e0d720bb443149d471aca
YuanChang <759337828@qq.com>
2021-05-09 15:55:29 +0800

    配置快速生成vue template

---
+++ src/components/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: '',
+  data () {
+    return {
+    }
+  },
+  components: {},
+  mounted () { },
+  methods: {}
+}
+</script>
+
+<style lang='stylus' scoped></style>
