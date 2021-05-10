1129613a2c482742766ad87ef7e11625af75086f
YuanChang <759337828@qq.com>
2021-05-10 11:11:49 +0800

    login表单的数据绑定

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <div id="app">app root</div>
+  <div id="app">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,5 +1,23 @@
 <template>
-  <div></div>
+  <div class="login-container">
+    <div class="login-box">
+      <div class="avatar-box">
+        <img src="../assets/logo.png" alt />
+      </div>
+      <el-form :model="loginForm" class="login-form" label-width="0px">
+        <el-form-item>
+          <el-input v-model="loginForm.username" prefix-icon="iconfont icon-user"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" v-model="loginForm.password" prefix-icon="iconfont icon-3702mima"></el-input>
+        </el-form-item>
+        <el-form-item class="btns">
+          <el-button type="primary">登录</el-button>
+          <el-button type="info">重置</el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -7,6 +25,10 @@ export default {
   name: '',
   data () {
     return {
+      loginForm: {
+        username: 'zs',
+        password: '123456'
+      }
     }
   },
   components: {},
@@ -15,4 +37,49 @@ export default {
 }
 </script>
 
-<style lang='stylus' scoped></style>
+<style lang='less' scoped>
+.login-container {
+	background-color: #2b4b6b;
+	height: 100%;
+}
+.login-box {
+	width: 450px;
+	height: 300px;
+	background-color: #fff;
+	border-radius: 3px;
+	position: absolute;
+	left: 50%;
+	top: 50%;
+	transform: translate(-50%, -50%);
+	.avatar-box {
+		height: 130px;
+		width: 130px;
+		border: 1px solid #eee;
+		border-radius: 50%;
+		padding: 10px;
+		box-shadow: 0 0 10px #ddd;
+		position: absolute;
+		left: 50%;
+		transform: translate(-50%, -50%);
+		background-color: #fff;
+		img {
+			width: 100%;
+			height: 100%;
+			border-radius: 50%;
+			background-color: #eee;
+		}
+	}
+}
+
+.btns {
+	display: flex;
+	justify-content: center;
+}
+.login-form {
+	position: absolute;
+	bottom: 0;
+	width: 100%;
+	padding: 0 20px;
+	box-sizing: border-box;
+}
+</style>
